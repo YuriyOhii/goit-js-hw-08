4f5c5780adfb6e2d270c12217f7a6a7ea9f3835c
YuriyOhii <uaogiy@gmail.com>
2023-05-22 11:19:54 +0300

    adds the second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+const throttle = require('lodash.throttle');
+import Player from '@vimeo/player';
 
+
+const CURRENT_TIME = "videoplayer-current-time";
+const player = new Player(document.querySelector('iframe'));
+
+player.setCurrentTime(localStorage.getItem("CURRENT_TIME"));
+player.on('timeupdate', throttle(onPlayerTimeupdate, 1000));
+
+function onPlayerTimeupdate(e) {
+    localStorage.setItem("CURRENT_TIME", e.seconds);
+    console.log(e.seconds);
+}
